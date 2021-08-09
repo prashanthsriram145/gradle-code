0df29ce3e41ee72580dc704768dea59d3df24fdd
prashanthsriram145 <prashanthsriram145@gmail.com>
2021-08-09 21:40:11 +0530

    initial commit

---
+++ src/main/java/com/sriram/gradlecode/GradleCodeApplication.java
@@ -0,0 +1,13 @@
+package com.sriram.gradlecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sriram/gradlecode/GradleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriram.gradlecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
